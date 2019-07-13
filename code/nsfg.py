4ad51ada1d5fdd731ad69d349b97c622874d907e
Shrivatsa <shrivatsa@artoo.in>
2019-07-13 17:02:31 +0530

    Chap01

--- code/nsfg.py
+++ code/nsfg.py
@@ -6,6 +6,7 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
 from __future__ import print_function
+from collections import defaultdict
 
 import numpy as np
 
@@ -29,7 +30,7 @@ def MakeFrames():
     return live, firsts, others
 
 
-def ReadFemPreg():
+def ReadFemPreg(dct_file, dat_file):
     """Reads the NSFG 2006-2010 pregnancy data.
 
     dct_file: string file name
@@ -53,23 +54,55 @@ def CleanFemPreg(df):
 
     # birthwgt_lb contains at least one bogus value (51 lbs)
     # replace with NaN
-    df.loc[df.birthwgt_lb1 > 20, 'birthwgt_lb1'] = np.nan
+    df.loc[df.birthwgt_lb > 20, 'birthwgt_lb'] = np.nan
     
     # replace 'not ascertained', 'refused', 'don't know' with NaN
     na_vals = [97, 98, 99]
-    df.birthwgt_lb1.replace(na_vals, np.nan, inplace=True)
-    df.birthwgt_oz1.replace(na_vals, np.nan, inplace=True)
+    df.birthwgt_lb.replace(na_vals, np.nan, inplace=True)
+    df.birthwgt_oz.replace(na_vals, np.nan, inplace=True)
 
     # birthweight is stored in two columns, lbs and oz.
     # convert to a single column in lb
     # NOTE: creating a new column requires dictionary syntax,
     # not attribute assignment (like df.totalwgt_lb)
-    df['totalwgt_lb'] = df.birthwgt_lb1 + df.birthwgt_oz1 / 16.0  
+    df['totalwgt_lb'] = df.birthwgt_lb + df.birthwgt_oz / 16.0  
 
     # due to a bug in ReadStataDct, the last variable gets clipped;
     # so for now set it to NaN
     df.phase = np.nan
 
+def MakePregMap(df):
+    """Make a map from caseid to list of preg indices.
+
+    df: DataFrame
+
+    returns: dict that maps from caseid to list of indices into `preg`
+    """
+    d = defaultdict(list)
+    for index, caseid in df.caseid.iteritems():
+        d[caseid].append(index)
+    return d
+
+def ReadFemResp(dct_file, dat_file, nrows=None):
+    """Reads the NSFG respondent data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+
+def CleanFemResp(df):
+    """Recodes variables from the respondent frame.
+
+    df: DataFrame
+    """
+    pass
 
 def main():
     live, firsts, others = MakeFrames()
